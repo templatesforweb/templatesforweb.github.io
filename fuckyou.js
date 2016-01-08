6cae9821fc6f8b814d0862f41d8909f4b1f9657a
templatesforweb <george01raikos@gmail.com>
2016-01-08 22:03:57 +0200

    Create fuckyou.js

---
+++ fuckyou.js
@@ -0,0 +1,7 @@
+function fuck() {
+var name = prompt("Hey.. what's your name?");
+alert("Fuck you " + name + " .");alert("Fuck you " + name + " .");alert("Fuck you " + name + " .");alert("Fuck you " + name + " .");alert("Fuck you " + name + " .");alert("Fuck you " + name + " .");alert("Fuck you " + name + " .");alert("Fuck you " + name + " .");alert("Fuck you " + name + " .");alert("Fuck you " + name + " .");alert("Fuck you " + name + " .");alert("Fuck you " + name + " .");alert("Fuck you " + name + " .");alert("Fuck you " + name + " .");alert("Fuck you " + name + " .");alert("Fuck you " + name + " .");alert("Fuck you " + name + " .");alert("Fuck you " + name + " .");alert("Fuck you " + name + " .");alert("Fuck you " + name + " .");alert("Fuck you " + name + " .");
+
+
+
+}
