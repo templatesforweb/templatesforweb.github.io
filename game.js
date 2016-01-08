08769d2e2a6f2ca9d34f03f2aeacdb0ed48c81cf
templatesforweb <george01raikos@gmail.com>
2016-01-08 21:56:56 +0200

    Create game.js

---
+++ game.js
@@ -0,0 +1,26 @@
+function game() {
+  var name = prompt("So... what's your name?");
+  alert("Hey, " + name + " " +"are you ready to play?");
+var age = prompt("How old are you?");
+  if (age >= 15) {
+    alert('Welcome!');
+    alert("Hellof romn");
+  
+  
+  
+  
+  
+
+  }
+  
+  
+  
+  
+  else {
+    alert("You're too young for this!"); 
+    window.close();
+  };
+  
+  
+  
+}
